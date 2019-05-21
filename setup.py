f8deb37d2c82fe53be2223f550099af1ee4b0d13
Arthurlpgc <arthurlpgcosta@gmail.com>
2019-05-21 03:37:02 -0300

    0.1.0

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'fieldy',         # How you named your package folder (MyLib)
   packages = ['fieldy'],   # Chose the same as "name"
-  version = '0.0.3',      # Start with a small number and increase it with every change you make
+  version = '0.1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Lib to help structure objects from jsons',   # Give a short description about your library
   author = 'arthurlpgc',                   # Type in your name
   author_email = 'arthurlpgc@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/NeverDefineUs/fieldy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/NeverDefineUs/fieldy/archive/0.0.3.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/NeverDefineUs/fieldy/archive/0.1.0.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
